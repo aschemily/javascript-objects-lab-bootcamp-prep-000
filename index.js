25e2c7e4b02f926e16224e4686049304f0659f0d
Emily Asch <e.asch@me.com>
2018-07-30 15:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = new Object ({})
 
 function updateObjectWithKeyAndValue(object, key, value){
  var obj = { prop: 1 }
-object.assign ({prop: 1}, obj,);
+object.assign ({}, obj,);
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
